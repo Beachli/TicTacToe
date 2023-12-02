48be41a57c479a5070a7b26b082ea0f7f02f769b
Liam Beach <beachliam48@gmail.com>
2023-12-02 15:39:32 -0600

    Draws a 3x3 grid

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include <raylib.h>
 #include <string>
 
+
+#define COLS 3
+#define ROWS 3
+
+const int screenHeight = 400;
+const int screenWidth = 400;
+const int cellWidth = GetScreenWidth() / COLS;
+const int cellHeight = GetScreenHeight() / ROWS;
+
+char board[3][3];
+
 int main() {
-  InitWindow(600, 600, "Tic-Tac-Toe");
+  InitWindow(screenWidth, screenHeight, "Tic-Tac-Toe");
   SetWindowState(FLAG_VSYNC_HINT);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
       ClearBackground(WHITE);
       
+      for (int i = 0; i < COLS; i++) {
+        for (int j = 0; j < ROWS; j++) {
+          DrawRectangleLines(i * cellWidth, j * cellHeight, cellWidth, cellHeight, BLACK);
+        }
+      }
+
     EndDrawing();
   }
 }
